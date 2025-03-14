1d89a4a3a8c36801e6ecd28c785abcba0ff9548f
Behrooz Firoozi <firooziukbehrooz@gmail.com>
2025-03-14 20:39:30 +0000

    DFGDGDGffffw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Banner from '@/components/ui/banner';
 import Welcome from '@/components/welcome';
+import Test from './test/page';
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
+
+      <Test />
       <Banner />
       <Welcome />
     </div>
