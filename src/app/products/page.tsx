1a57a7af1cd4c19c945aa625dcfbe9ca5193e2b8
Behrooz Firoozi <firooziukbehrooz@gmail.com>
2025-03-16 23:22:26 +0000

    سیسبسبسب

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,14 @@
+import { prisma, PrismaType } from '@/lib/prisma';
 import ProductListView from '@/modules/products/views/ProductListView';
 import React from 'react';
 
-const Products = () => {
-  return <div>
+async function Products () {
+
+  const data: PrismaType.Product[] = await prisma.product.findMany()
+  console.log(data);
+
+  return 
+  <div>
     <ProductListView/>
   </div>;
 };
