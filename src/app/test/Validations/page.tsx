fd260156e5e6909d5e8b26af0b7ebd510bc43761
Behrooz Firoozi <firooziukbehrooz@gmail.com>
2025-03-16 19:45:37 +0000

    sfsdfsdfs

--- src/app/test/Validations/page.tsx
+++
@@ -1,9 +0,0 @@
-  import * as yup from 'yup';
-
-  const userSchema = yup.object().shape({
-    name: yup.string().required('Name is required'),
-    email: yup.string().email('Invalid email').required('Email is required'),
-    password: yup.string().min(4, 'Password must be at least 4 characters').max(10, 'Password must be less than 10 characters').required('Password is required'),
-  })
-
-  export default userSchema;
