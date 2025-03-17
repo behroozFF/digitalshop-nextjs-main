9a44bc49f0e9e2b06b76873313e87530a65f6827
Behrooz Firoozi <firooziukbehrooz@gmail.com>
2025-03-17 10:33:45 +0000

    git commit add branch  new-feature

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,7 +2,7 @@ import { prisma, PrismaType } from '@/lib/prisma';
 import ProductListView from '@/modules/products/views/ProductListView';
 import React from 'react';
 
-async function Products () {
+async function Products () { 
 
   const data: PrismaType.Product[] = await prisma.product.findMany()
   console.log(data);
